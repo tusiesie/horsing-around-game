71ed3d85425ddffa35fc3875e254d54b59ded705
root <root@DESKTOP-ERH9GVB.>
2025-02-23 15:33:26 -0800

    file cleanup

--- code/gameFunctions.py
+++ code/gameFunctions.py
@@ -1,47 +1,7 @@
 import pygame
-import time
 
 from sceneManager import Background
-# starts a new game
-def show_game(screen):
-    return "game"
 
-'''
-class Fader:
-
-    def __init__(self, scenes):
-        self.scenes = cycle(scenes)
-        self.scene = next(self.scenes)
-        self.fading = None
-        self.alpha = 0
-        sr = pygame.display.get_surface().get_rect()
-        self.veil = pygame.Surface(sr.size)
-        self.veil.fill((0, 0, 0))
-
-    def next(self):
-        if not self.fading:
-            self.fading = 'OUT'
-            self.alpha = 0
-
-    def draw(self, screen):
-        self.scene.draw(screen)
-        if self.fading:
-            self.veil.set_alpha(self.alpha)
-            screen.blit(self.veil, (0, 0))
-
-    def update(self, dt, events):
-        self.scene.update(dt, events)
-
-        if self.fading == 'OUT':
-            self.alpha += 8
-            if self.alpha >= 255:
-                self.fading = 'IN'
-                self.scene = next(self.scenes)
-        else:
-            self.alpha -= 8
-            if self.alpha <= 0:
-                self.fading = None
-'''
 class Transitions:
     def __init__(self):
         self.alpha = 0
@@ -79,6 +39,10 @@ class Transitions:
 
 
 T = Transitions()
+# starts a new game
+def show_game(screen):
+    return "game"
+
 def change_screen(name, screen, prev_bg):
     if name == "game":
         bg = pygame.image.load(Background.HIGHSCHOOL_STAIRS.value)
