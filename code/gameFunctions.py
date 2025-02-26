1be483846440eafb01b4c73e10e28a8592b0c87f
root <root@DESKTOP-ERH9GVB.>
2025-02-25 20:35:23 -0800

    render dialogue box

--- code/gameFunctions.py
+++ code/gameFunctions.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import pygame.freetype
 # from os import WSTOPSIG
@@ -42,16 +43,68 @@ class Transitions:
     def typewriter(self, text):
         pass
 
+class DialogueBox:
+    def set_dialogue_text_box(self, dialogue_type, character):
+        text_boxes = os.path.join(os.path.dirname(__file__), '../assets/visuals/textBoxes/')
+        path = ''
+        if dialogue_type == 'default':
+            if character == 'MC':
+                path = text_boxes + 'textBoxLeftStars.png'
+            else: path = text_boxes + 'textBoxRightStars.png'
 
+        elif dialogue_type == 'hearts':
+            if character == 'MC':
+                path = text_boxes + 'textBoxLeftHearts.png'
+            else: path = text_boxes + 'textBoxRightHearts.png'
+
+        else: raise Exception('Unknown type for TextBox')
+        self.dialogue_box = pygame.image.load(path)
+
+    def __init__(self, dialogue_box_type = 'default', character = 'MC', action=None):
+
+        self.dialogue_box = None
+        self.set_dialogue_text_box(dialogue_box_type, character) # initialize dialogue box
+
+        self.screen_size = pygame.display.set_mode().get_size()
+        self.action = action
+
+
+    def draw(self, surface):
+        #if self.rect.collidepoint(pygame.mouse.get_pos()):
+        #    surface.blit(self.image_hovered, self.rect.topleft)
+        #else:
+        #    surface.blit(self.image_normal, self.rect.topleft)
+
+        size = pygame.transform.scale(self.dialogue_box, (900, 500))
+        rect = size.get_rect(topleft=(500, 500)) 
+        #self.rect = self.dialogue_box.get_rect(center=(x//2, y - 100))
+
+        surface.blit(size, (500, 600))
+    def is_clicked(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+            if self.action:
+                self.action()
+            return True
+        return False
 T = Transitions()
+D = DialogueBox()
 ctx = Context()
+
+def show_dialogue_box():
+    pass
 
 # starts a new game
 def show_game(screen):
+
     ctx.load_arc(Arcs.BEGINNING)
+
     character, line = next(ctx.get_dialogue())
+
+    
+    D.draw(screen) # draw dialogue box
     text_surface, rect = ctx.font.render( f'{character}: {line}', (0,0,0) )
     screen.blit(text_surface, (40, 250))
+
     return "game"
 
 def show_saved(screen):
